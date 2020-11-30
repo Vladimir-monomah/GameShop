6c54cc2ba913205e1fe40c9673687d2a4b7d5c7b
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2020-11-30 18:30:25 +0300

    1. Создание решения и проектов Visual Studio 2. Настройка контейнера DI 3. Модель предметной области 4. Создание абстрактного хранилища 5. Создание имитированного хранилища 6. Добавление контроллера 7. Добавление компоновки, файла запуска представления и самого представления. Визуализация данных представления 8. Установка стандартного маршрута 9. Создание БД 10. Создание контекста Entity Framework 11. Создание хранилища для объектов Game

---
+++ GameStore/GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using Moq;
+using GameStore.Domain.Concrete;
+
+namespace GameStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver:IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            this.kernel = kernelParam;
+            this.AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return this.kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return this.kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IGameRepository>().To<EFGameRepository>();
+        }
+    }
+}
