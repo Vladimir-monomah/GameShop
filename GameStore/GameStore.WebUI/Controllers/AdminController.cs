3bd2149168f3858528d50702f7fa51e0132fea95
Vladimir Bykov <vladimir-bykov-1999@mail.ru>
2020-12-07 14:18:17 +0300

    1. Создание контроллера CRUD 2. Создание новой компоновки 3. Реализация представления списка

---
+++ GameStore/GameStore.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using GameStore.Domain.Abstract;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class AdminController : Controller
+    {
+        IGameRepository repository;
+
+        public AdminController(IGameRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View(this.repository.Games);
+        }
+    }
+}
